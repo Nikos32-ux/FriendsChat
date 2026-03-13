3641104911f20559166f29124ff5a1cdc21430c9
nikosMos <moschos30@hotmail.com>
2026-03-13 10:50:03 +0200

    User component sidebar tailwind update for message display

--- frontend/src/components/User.jsx
+++ frontend/src/components/User.jsx
@@ -11,7 +11,7 @@ const User = ({ user, isOnline, currentUser, selectedUser, lastMessages, sidebar
                 <img src={user.profile} alt={user.name} className="w-8 h-8 sm:w-10 sm:h-10 rounded-full object-cover transition-all duration-300" />
                 <span className={`absolute -bottom-1 -right-0 w-3 h-3 rounded-full ${isOnline ? "bg-green-500" : "bg-red-500"}`} />
             </div>
-            <div className="flex flex-1 flex-col justify-start">
+            <div className="flex flex-1 flex-col justify-center min-w-0">
                 <span className="font-medium text-white text-base sm:text-lg  ">
                     {user.name}
                 </span>
@@ -20,30 +20,33 @@ const User = ({ user, isOnline, currentUser, selectedUser, lastMessages, sidebar
                         {`${user.name.split(" ")[0]} is typing...`}
                     </span>
                 ) : (
-                    <div className="font-normal flex items-center text-sm gap-2 w-full ">
-                        <p className=" truncate min-w-0 text-gray-200 ">
-                            {
-                                lastMessages?.message
-                                    ? currentUser._id === lastMessages?.userId
-                                        ? <>
-                                            <span className='flex items-center '>
-                                                <span className='mr-1'>You : </span>
-                                                <span className='truncate ml-2 text-sm md:text-xl max-w-[330px]'>
-                                                    {lastMessages?.message}
+                    <div className="font-normal flex items-center gap-2 w-full">
+                        <div className='flex items-center gap-1.5 min-w-0 overflow-hidden'>
+                            <div className=" min-w-0 text-gray-200 text-sm ">
+                                {
+                                    lastMessages?.message
+                                        ? currentUser._id === lastMessages?.userId
+                                            ? <>
+                                                <span className='flex items-center '>
+                                                    <span className='shrink-0'>You : </span>
+                                                    <span className='truncate ml-1 text-sm'>
+                                                        {lastMessages?.message}
+                                                    </span>
                                                 </span>
+                                            </>
+                                            : <span className='flex'>
+                                                <span className={`truncate ${lastMessages.bold ? "font-bold" : "text-white"} truncate max-w-[300px] sm:max-w[350px]`}>{lastMessages?.message}</span>
                                             </span>
-                                        </>
-                                        : <span className='flex'>
-                                            <span className={`${lastMessages.bold ? "font-bold" : "text-white"} truncate max-w-[300px] sm:max-w[350px]`}>{lastMessages?.message}</span>
-                                        </span>
-                                    : "No message yet"
-                            }
-                        </p>
-                        {currentUser._id === lastMessages?.userId ? (
-                            <span className={` shrink-0 ${lastMessages?.sidebarRead ? "text-[rgba(158,10,42,0.82)]" : "text-[rgba(110,111,121,0.82)]"}`}>
-                                ✓✓
-                            </span>
-                        ) : ""}
+                                        : "No message yet"
+                                }
+                            </div>
+                            {currentUser._id === lastMessages?.userId ? (
+                                <span className={` text-[15px] shrink-0 ${lastMessages?.sidebarRead ? "text-[rgba(158,10,42,0.82)]" : "text-[rgba(110,111,121,0.82)]"}`}>
+                                    ✓✓
+                                </span>
+                            ) : ""}
+                        </div>
+
                     </div>
                 )}
             </div>
